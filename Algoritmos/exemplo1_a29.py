aafe645e44536a56245a9a671b73688ac24e2f79
JoseVinGarcia <josevingarcia2020@gmail.com>
2025-01-07 20:24:28 -0300

    Exemplos 1 e 2

---
+++ Algoritmos/exemplo1_a29.py
@@ -0,0 +1,18 @@
+# AULA 29 - PYLANCE E TESTE COM GITHUB COPILOT
+# APOS INSTALAR O COPILOT, ele irá completar o código debaixo dos comentarios que voce por.
+
+# IMportar pandas
+import pandas as pd
+
+# criar dataframe
+df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+
+# obter dados de um arquivo csv
+# df = pd.read_csv('file.csv')
+
+# imprimir dataframe chamado df
+print(df)
+
+# criando um dataframe
+df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+
